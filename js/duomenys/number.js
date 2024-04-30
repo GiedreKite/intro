1c88e483ccfa82e0ad3014d3632414356534f050
Giedre <GIEDRE.NARVILAITE@GEMAIL.COM>
2024-04-30 13:54:32 +0300

    js/duomenys/number.js

---
+++ js/duomenys/number.js
@@ -0,0 +1,136 @@
+/*
+Numbers
+- sveikieji / desimtainiai
+- teigiami / nulis / neigiami
+- normalus / ne normalus
+
+NE normalus:
+- Infinity
+- -Infinity
+- NaN (not-a-number)
+
+Matematiniai operatoriai:
+- aritmetiniai +, -, /, *, **(laipsniai), komutatyvumas(5*7 ar 7*5, negalioja kitiems veiksmams, kur skirtumas yra atsakyme)
+- priskyrimo =+, =-, =/, =*, =**(laipsniai), komutatyvumas(5*7 ar 7*5, negalioja kitiems veiksmams, kur skirtumas yra atsakyme)
+
+
+*/
+
+const pirmas = 5;
+const antras = 7;
+const trecias = -2.727;
+const nulis = 0;
+const asd = 999999;
+
+
+
+console.log(3.1415);
+console.log(pirmas);
+console.log(antras);
+console.log(trecias);
+console.log(nulis);
+console.log(asd);
+console.log(asd);
+console.log(asd);
+
+const suma = pirmas + antras;
+console.log(suma);
+
+const skirtumas = pirmas - antras;
+console.log(skirtumas);
+
+const dalmuo = pirmas / antras;
+console.log(dalmuo);
+
+const dalmuo2 = antras/ pirmas;
+console.log(dalmuo2);
+
+const sandauga = pirmas * antras;
+console.log(sandauga);
+
+const laipsnis = pirmas ** antras;
+console.log(laipsnis);
+
+const laipsnis2 = antras ** pirmas;
+console.log(laipsnis2);
+
+console.log(2**2, 2*2);
+console.log(2**3, 2*2*2);
+console.log(2**4, 2*2*2*4);
+
+const liekana = pirmas % antras;
+console.log(liekana);
+
+const liekana2 = antras % pirmas;
+console.log(liekana2);
+
+//console.clear()
+
+let pinigine = 0;
+console.log(pinigine);
+
+pinigine = 10;
+console.log(pinigine);
+
+pinigine = 50;
+console.log(pinigine);
+console.log(pinigine);
+
+console.clear()
+
+let money = 0;
+console.log(money);
+
+money = 5;
+console.log(money);
+
+money = money + 24;
+console.log(money);
+
+money = money + 1;
+console.log(money);
+
+
+money = 69;
+console.log(money);
+
+console.clear()
+
+//x = x + 1; x = x + 1;
+
+let x = 1;
+x += 1;
+console.log(x);
+
+x *= 20;
+console.log(x);
+
+x /= 5;
+console.log(x);
+
+x %= 3;
+console.log(x);
+
+
+let y = 10;
+y += 10 +10 ;
+console.log(y);
+
+y -= 5 * 4;
+console.log(y);
+
+y -= 10 + 5 * 4;
+console.log(y);
+
+
+y /=5;
+console.log(y);
+
+y %=3;
+console.log(y);
+
+
+
+
+
+
